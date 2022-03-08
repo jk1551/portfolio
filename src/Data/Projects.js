965c7e6535131cea98f28c5275e590aa2f4117cc
Joe King <joe.k659@gmail.com>
2022-03-08 16:52:22 -0600

    pages

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -7,19 +7,19 @@ const projects = [
       href: 'https://github.com/jk1551/portfolio',
     },
     {
+        name: 'Motosho Forms',
+        description: 'React, Material UI - Live Site Page',
+        imageSrc: 'https://images.pexels.com/photos/10550012/pexels-photo-10550012.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        imageAlt: 'alternative',
+        href: 'https://motosho.com/add-club',
+      },
+    {
       name: 'Golf Website',
       description: 'React, Styled Components',
       imageSrc: 'https://images.pexels.com/photos/6256752/pexels-photo-6256752.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       imageAlt: 'Golf',
       href: 'https://github.com/JoeKing-Clients/Jonny-Montes-Golf',
     },
-    {
-      name: 'Contact Card Generator',
-      description: 'React, Tailwind, Axios',
-      imageSrc: 'https://images.pexels.com/photos/326576/pexels-photo-326576.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      imageAlt: 'Project',
-      href: 'https://github.com/jk1551/Contact-Card',
-    }
   ]
 
   export default projects;
